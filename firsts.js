05f119c4a9f966cdf0cf51b9ce5a94611b4f4b61
Allie Etcoff <aetcoff@zappos.com>
2014-07-01 15:32:49 -0700

    version2.1

--- firsts.js
+++ firsts.js
@@ -1,11 +1,6 @@
 
 var listNav = $('#list-nav');
 
-
-  $('#add-new-icon').click(function() {
-      console.log("Hello");
-  });
-
  // inserts content from add-new-container to list-nav
 
 $('#add-new-icon').hover(
@@ -16,27 +11,12 @@ $('#add-new-icon').hover(
     $(this).removeClass('hover');
   });
 
-
 $('form').submit(function() {
-  if ($('input').val() !== '') {
-    var input_value = $('input').val();
-    var markup = [];
-      markup.push (
-        '<li>',
-        '<a class="checkbox" href="#">checkbox</a>',
-        '<span class="new-list-item">' + input_value + '</span>',
-        '<a class="remove" href="#">remove</a>',
-        '</li>');
 
-    listNav.prepend(markup.join(''));
-  };
 
 });
 
-$(document).on('click', 'a.remove', function(e){
-  e.preventDefault();
-  $(this).parent().remove();
-});
+
 
 // opens/closes add-new-container
 
@@ -52,10 +32,88 @@ $('#list-nav').click(function(e) {
   if (e.target.className === 'checkbox'){
     if (parent.hasClass('completed')) {
       parent.removeClass('completed');
+
     }
     else { parent.addClass('completed');
   };
   };
 });
+
+// getting tag values
+
+
+
+//remove placeholder text on focus
+$('input,textarea').focus(function(){
+   $(this).removeAttr('placeholder');
+});
+
+// show and hide .new-tag-plus on click of .tag-icon-plus
+$('.tag-icon-plus').click(function() {
+  $('.new-tag-plus').toggle();
+});
+
+//local storage
+
+$(document).ready(function(ls){
+  var firsts;
+  if (ls.getItem('firsts')) {
+    firsts = JSON.parse(ls.getItem('firsts'));
+    buildList(firsts);
+  } else {
+    ls.setItem('firsts','{}');
+  }
+
+  var todo;
+  $('#add-button').click(function(e){
+    e.preventDefault();
+    todo = $('input')[0].value;
+    var inputs = $('input.input-tag');
+    tagsArr = [];
+    for (var i = 0; i < inputs.length; i++) {
+      console.log(i, inputs, inputs[i]);
+      tagsArr.push(inputs[i].value);
+    }
+    firsts[todo] = {
+      'todo': todo,
+      'completed': false,
+      'tags': tagsArr
+    };
+    ls.setItem('firsts', JSON.stringify(firsts));
+    if ($('#new-firsts').val() !== '') {
+      var input_value = $('#new-firsts').val();
+      var markup = [];
+
+      console.log(tagsArr);
+        markup.push (
+          '<li>',
+          '<a class="checkbox" href="#">checkbox</a>',
+          '<span class="new-list-item">' + input_value + '</span>',
+          '<a class="remove" href="#">remove</a>',
+          '<span class="list-tag-item">' + tagsArr.join(' ,') + '</span>',
+          '</li>');
+
+      listNav.prepend(markup.join(''));
+    };
+  });
+
+  //remove item
+
+  $('#list-nav').on('click', '.remove', function(e){
+    e.preventDefault();
+    var key = $(this).parent().find("span").text();
+    delete firsts[key];
+    $(this).parent().remove();
+    ls.setItem('firsts', JSON.stringify(firsts));
+
+  });
+
+  function buildList(firsts) {
+    for (foo in firsts) {
+      $('#list-nav').prepend('<li><a class="checkbox" href="#">checkbox</a><span class="new-list-item">'+ firsts[foo].todo + '</span> <a class="remove" href="#">remove</a></li>');
+    }
+  }
+}(window.localStorage));
+
 
 
